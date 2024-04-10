85e2c3c4668f831cfa7b62d3086ae976fddb7d1d
lethucthanhtu <lethucthanhtu@gmail.com>
2024-04-10 14:10:33 +0000

    fix router on production

--- vite.config.ts
+++ vite.config.ts
@@ -3,19 +3,19 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    host: 'localhost', 
-  }, 
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: ["react", "react-router", "react-router-dom", "react-redux"],
-      output: {
-        globals: {
-          react: "React",
-        },
-      },
-    },
-  }
+  // server: {
+  //   host: 'localhost', 
+  // }, 
+  // build: {
+  //   rollupOptions: {
+  //     external: ['react', 'react-router', 'react-router-dom', 'react-redux'],
+  //     output: {
+  //       globals: {
+  //         react: 'React',
+  //       },
+  //     },
+  //   },
+  // }
 })
 
