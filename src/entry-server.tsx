955caca3050430622ab2cbf980b7648e13b40ab8
le tu <lethucthanhtu@gmail.com>
2024-04-14 19:20:55 +0700

    Test server side rendering

--- src/entry-server.tsx
+++ src/entry-server.tsx
@@ -1,11 +1,17 @@
 import React from 'react'
 import ReactDOMServer from 'react-dom/server'
 import App from './App'
+import { ThemeProvider } from '@material-tailwind/react';
+import { StaticRouter as Router } from 'react-router-dom/server';
 
 export function render() {
   const html = ReactDOMServer.renderToString(
     <React.StrictMode>
-      <App />
+        <ThemeProvider>
+            <Router location={'/'}>
+                <App />
+            </Router>
+        </ThemeProvider>
     </React.StrictMode>
   )
   return { html }
