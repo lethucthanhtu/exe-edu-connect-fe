0ad6fc2e905ea498a1066f897cdbccca4508e507
le tu <lethucthanhtu@gmail.com>
2024-04-15 18:15:29 +0700

    test ssr

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -24,5 +24,6 @@ module.exports = {
                 reportUsedIgnorePattern: false,
             },
         ],
+        'no-undef': 'off',
     },
 };
--- src/i18n/i18n.ts
+++ src/i18n/i18n.ts
@@ -27,6 +27,7 @@ export default i18n
                     'log in': 'đăng nhập',
                     'sign up': 'đăng ký',
                     search: 'tim kiếm',
+                    'Go back to Home':'Về trang chủ'
                 },
             },
         },
