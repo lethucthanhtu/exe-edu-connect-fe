72b41847af8e178b34531dcdb0a6863d9e2d99ae
le tu <lethucthanhtu@gmail.com>
2024-04-15 19:25:57 +0700

    test search bar routing

--- src/entry-server.tsx
+++ src/entry-server.tsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import ReactDOMServer from 'react-dom/server'
+import React from 'react';
+import ReactDOMServer from 'react-dom/server';
 import { ThemeProvider } from '@material-tailwind/react';
 
 import App from './App';
@@ -7,12 +7,12 @@ import './index.css';
 import './i18n/i18n';
 
 export function render() {
-  const html = ReactDOMServer.renderToString(
-      <React.StrictMode>
-          <ThemeProvider>
-              <App />
-          </ThemeProvider>
-      </React.StrictMode>
-  );
-  return { html }
+    const html = ReactDOMServer.renderToString(
+        <React.StrictMode>
+            <ThemeProvider>
+                <App />
+            </ThemeProvider>
+        </React.StrictMode>
+    );
+    return { html };
 }
