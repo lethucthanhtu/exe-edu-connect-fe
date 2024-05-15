443409bf830c24f39d3e4f82e9d0690ad8c8d217
le tu <lethucthanhtu@gmail.com>
2024-05-15 21:03:40 +0700

    fix step 2 forgot password page

--- src/pages/secure/forgotPassword.tsx
+++ src/pages/secure/forgotPassword.tsx
@@ -9,6 +9,176 @@ import {
 import { useTranslation } from 'react-i18next';
 import { Link } from 'react-router-dom';
 import { capitalize } from '../../utils/utils';
+import { InputEmail, InputPassword } from '../../components/input';
+
+type TFormProps = {
+  activeStep: number;
+  handleNext?: any;
+};
+
+/** */
+function FormStep1({ activeStep, handleNext }: TFormProps) {
+  const { t } = useTranslation();
+  return (
+    <>
+      <form
+        action='#'
+        className={`size-full flex flex-col gap-4 justify-center p-auto items-center
+                        ${activeStep === 0 ? 'animate-fade-right' : 'hidden'}
+                      `}
+      >
+        <Typography
+          variant='h3'
+          color='teal'
+          className='mb-4'
+          placeholder={undefined}
+          onPointerEnterCapture={undefined}
+          onPointerLeaveCapture={undefined}
+        >
+          {capitalize(t('forgot password'))}
+        </Typography>
+
+        <div className='flex flex-col justify-center items-center w-full gap-1'>
+          <div className='flex flex-col gap-6 items-center w-full h-auto'>
+            <InputEmail color='teal' />
+            <Typography
+              className=''
+              placeholder={undefined}
+              onPointerEnterCapture={undefined}
+              onPointerLeaveCapture={undefined}
+            >
+              Vui lòng nhập email để nhận mã OTP
+            </Typography>
+          </div>
+        </div>
+
+        <Button
+          className='mt-12 w-full bg-secondary-midBlue'
+          onClick={handleNext}
+          placeholder={undefined}
+          onPointerEnterCapture={undefined}
+          onPointerLeaveCapture={undefined}
+        >
+          {t('continue')}
+        </Button>
+      </form>
+    </>
+  );
+}
+
+/** */
+function FormStep2({ activeStep, handleNext }: TFormProps) {
+  const { t } = useTranslation();
+  return (
+    <>
+      <form
+        action='#'
+        className={`size-full flex flex-col gap-4 justify-center p-auto items-center
+                        ${activeStep === 1 ? 'animate-fade-right' : 'hidden'}
+                      `}
+      >
+        <Typography
+          variant='h3'
+          color='teal'
+          className='mb-4'
+          placeholder={undefined}
+          onPointerEnterCapture={undefined}
+          onPointerLeaveCapture={undefined}
+        >
+          {capitalize(t('forgot password'))}
+        </Typography>
+
+        <div className='flex flex-col gap-4 justify-center items-center w-full'>
+          <div className='flex gap-2 items-center w-full h-auto'>
+            <InputEmail color='teal' />
+            <Button
+              className='basis-2/5 w-full text-primary'
+              variant='text'
+              placeholder={undefined}
+              onPointerEnterCapture={undefined}
+              onPointerLeaveCapture={undefined}
+            >
+              {t('resend code')}
+            </Button>
+          </div>
+
+          <Typography
+            className='w-full flex justify-start italic'
+            placeholder={undefined}
+            onPointerEnterCapture={undefined}
+            onPointerLeaveCapture={undefined}
+          >
+            Mã OTP sẽ được gửi trong giây lát
+          </Typography>
+
+          <Input
+            id=''
+            color='teal'
+            type='text'
+            name='OTP'
+            className='w-full'
+            label={t('enter OTP code')}
+            labelProps={{ className: 'capitalize' }}
+            onPointerEnterCapture={undefined}
+            onPointerLeaveCapture={undefined}
+            crossOrigin={undefined}
+          />
+        </div>
+        <Button
+          className='w-full bg-secondary-midBlue'
+          onClick={handleNext}
+          placeholder={undefined}
+          onPointerEnterCapture={undefined}
+          onPointerLeaveCapture={undefined}
+        >
+          {t('continue')}
+        </Button>
+      </form>
+    </>
+  );
+}
+
+/** */
+function FormStep3({ activeStep }: TFormProps) {
+  const { t } = useTranslation();
+  return (
+    <>
+      <form
+        action='#'
+        className={`size-full flex flex-col gap-4 justify-center p-auto items-center
+                        ${activeStep === 2 ? 'animate-fade-right' : 'hidden'}
+                      `}
+      >
+        <Typography
+          variant='h3'
+          color='teal'
+          className='mb-4'
+          placeholder={undefined}
+          onPointerEnterCapture={undefined}
+          onPointerLeaveCapture={undefined}
+        >
+          {capitalize(t('forgot password'))}
+        </Typography>
+        <InputPassword color='teal' />
+        <InputPassword
+          color='teal'
+          isConfirmPassword='true'
+          isShowCapsLockAlert='false'
+        />
+        <Link className='w-full mt-12' to='/login'>
+          <Button
+            className='w-full bg-secondary-midBlue'
+            placeholder={undefined}
+            onPointerEnterCapture={undefined}
+            onPointerLeaveCapture={undefined}
+          >
+            {t('done')}
+          </Button>
+        </Link>
+      </form>
+    </>
+  );
+}
 
 type TStepProps = {
   totalSteps?: number;
@@ -20,20 +190,16 @@ type TStepProps = {
  */
 export default function ForgotPassword() {
   const { t } = useTranslation();
-  const [passwordShown, setPasswordShown] = useState(false);
-  const togglePasswordVisiblity = () => setPasswordShown((cur) => !cur);
   const [activeStep, setActiveStep] = useState(0);
   const [isLastStep, setIsLastStep] = useState(false);
   const [isFirstStep, setIsFirstStep] = useState(false);
-  const steps = [...Array(3).keys()];
 
   const handleNext = () => !isLastStep && setActiveStep((cur) => cur + 1);
   const handlePrev = () => !isFirstStep && setActiveStep((cur) => cur - 1);
 
   return (
     <>
-      {/*
-       <section className=''>
+      {/* <section className=''>
         <div className='w-full py-4 px-8'>
           <Stepper
             activeStep={activeStep}
@@ -280,16 +446,20 @@ export default function ForgotPassword() {
         <div className='w-full row-span-2 flex justify-center items-center py-4 px-8'>
           <Stepper
             className=''
+            color='teal'
             activeStep={activeStep}
+            activeLineClassName='bg-primary'
             isLastStep={(value) => setIsLastStep(value)}
             isFirstStep={(value) => setIsFirstStep(value)}
             placeholder={undefined}
             onPointerEnterCapture={undefined}
             onPointerLeaveCapture={undefined}
           >
-            {steps.map((_, index) => (
+            {[...Array(3).keys()].map((_, index) => (
               <Step
                 className=''
+                color='teal'
+                activeClassName='bg-primary'
                 onClick={() => setActiveStep(index)}
                 placeholder={undefined}
                 onPointerEnterCapture={undefined}
@@ -300,176 +470,9 @@ export default function ForgotPassword() {
             ))}
           </Stepper>
         </div>
-        <form
-          action='#'
-          className={`size-full flex flex-col gap-4 justify-center p-auto items-center
-                        ${activeStep === 0 ? 'animate-fade-right' : 'hidden'}
-                      `}
-        >
-          <Typography
-            variant='h3'
-            color='teal'
-            className='mb-4'
-            placeholder={undefined}
-            onPointerEnterCapture={undefined}
-            onPointerLeaveCapture={undefined}
-          >
-            {capitalize(t('forgot password'))}
-          </Typography>
-
-          <div className='flex flex-col justify-center items-center w-full gap-1'>
-            <div className='flex flex-col gap-6 items-center w-full h-auto'>
-              <Input
-                id='email'
-                type='email'
-                name='email'
-                color='teal'
-                label='Email'
-                onPointerEnterCapture={undefined}
-                onPointerLeaveCapture={undefined}
-                crossOrigin={undefined}
-              />
-              <Typography
-                className=''
-                placeholder={undefined}
-                onPointerEnterCapture={undefined}
-                onPointerLeaveCapture={undefined}
-              >
-                Vui lòng nhập email để nhận mã OTP
-              </Typography>
-            </div>
-          </div>
-
-          <Button
-            className='mt-12 w-full bg-secondary-midBlue'
-            onClick={handleNext}
-            placeholder={undefined}
-            onPointerEnterCapture={undefined}
-            onPointerLeaveCapture={undefined}
-          >
-            {t('continue')}
-          </Button>
-        </form>
-
-        <form
-          action='#'
-          className={`size-full flex flex-col gap-14 justify-center p-auto items-center
-                        ${activeStep === 1 ? 'animate-fade-right' : 'hidden'}
-                      `}
-        >
-          <Typography
-            variant='h3'
-            color='white'
-            className='mb-4'
-            placeholder={undefined}
-            onPointerEnterCapture={undefined}
-            onPointerLeaveCapture={undefined}
-          >
-            {t('forgot password')}
-          </Typography>
-
-          <div className='flex flex-col gap-4 justify-center items-center w-full'>
-            <div className='flex gap-2 items-center w-full h-auto'>
-              <Input
-                id='email'
-                color='teal'
-                type='email'
-                name='email'
-                className='w-full'
-                label={capitalize(t('Entered email'))}
-                onPointerEnterCapture={undefined}
-                onPointerLeaveCapture={undefined}
-                crossOrigin={undefined}
-              />
-              <Button
-                className='basis-2/5 w-full text-primary'
-                variant='text'
-                placeholder={undefined}
-                onPointerEnterCapture={undefined}
-                onPointerLeaveCapture={undefined}
-              >
-                {t('resend code')}
-              </Button>
-            </div>
-
-            <Typography
-              className='w-full flex justify-start italic'
-              placeholder={undefined}
-              onPointerEnterCapture={undefined}
-              onPointerLeaveCapture={undefined}
-            >
-              Mã OTP sẽ được gửi trong giây lát
-            </Typography>
-
-            <Input
-              id=''
-              color='teal'
-              type='text'
-              name='OTP'
-              className='w-full'
-              label={t('enter OTP code')}
-              labelProps={{ className: 'capitalize' }}
-              onPointerEnterCapture={undefined}
-              onPointerLeaveCapture={undefined}
-              crossOrigin={undefined}
-            />
-          </div>
-          <Button
-            className='w-full bg-secondary-midBlue'
-            onClick={handleNext}
-            placeholder={undefined}
-            onPointerEnterCapture={undefined}
-            onPointerLeaveCapture={undefined}
-          >
-            {t('continue')}
-          </Button>
-        </form>
-
-        <form
-          action='#'
-          className={`size-full flex flex-col gap-4 justify-center p-auto items-center
-                        ${activeStep === 2 ? 'animate-fade-right' : 'hidden'}
-                      `}
-        >
-          <Typography
-            variant='h3'
-            color='teal'
-            className='mb-4'
-            placeholder={undefined}
-            onPointerEnterCapture={undefined}
-            onPointerLeaveCapture={undefined}
-          >
-            {capitalize(t('forgot password'))}
-          </Typography>
-          <Input
-            color='teal'
-            label={capitalize(t('new password'))}
-            className='w-full'
-            type={passwordShown ? 'text' : 'password'}
-            onPointerEnterCapture={undefined}
-            onPointerLeaveCapture={undefined}
-            crossOrigin={undefined}
-          />
-          <Input
-            color='teal'
-            label={capitalize(t('reenter new password'))}
-            className='w-full'
-            type={passwordShown ? 'text' : 'password'}
-            onPointerEnterCapture={undefined}
-            onPointerLeaveCapture={undefined}
-            crossOrigin={undefined}
-          />
-          <Link className='w-full mt-12' to='/login'>
-            <Button
-              className='w-full bg-secondary-midBlue'
-              placeholder={undefined}
-              onPointerEnterCapture={undefined}
-              onPointerLeaveCapture={undefined}
-            >
-              {t('done')}
-            </Button>
-          </Link>
-        </form>
+        <FormStep1 activeStep={activeStep} handleNext={handleNext} />
+        <FormStep2 activeStep={activeStep} handleNext={handleNext} />
+        <FormStep3 activeStep={activeStep} handleNext={handleNext} />
       </section>
     </>
   );
