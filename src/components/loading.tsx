bfb896160dffab14bf4a457acda8adff10a2e186
le tu <lethucthanhtu@gmail.com>
2024-06-23 11:50:34 +0700

    lethucthanhtu: add middle prop

--- src/components/loading.tsx
+++ src/components/loading.tsx
@@ -1,9 +1,16 @@
 import { Spinner, SpinnerProps } from '@material-tailwind/react';
 
+type TLoadingProps = {
+  middle?: boolean;
+  relative?: boolean;
+} & SpinnerProps;
+
 /** */
-export default function Loading({ ...props }: SpinnerProps) {
+export default function Loading({ middle = false, ...props }: TLoadingProps) {
   return (
-    <>
+    <span
+      className={middle ? `size-full flex justify-center items-center` : ''}
+    >
       <Spinner
         {...props}
         color='teal'
@@ -11,6 +18,6 @@ export default function Loading({ ...props }: SpinnerProps) {
         onPointerEnterCapture={undefined}
         onPointerLeaveCapture={undefined}
       />
-    </>
+    </span>
   );
 }
