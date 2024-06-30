0bf5826fdfac29c477c72e409f955d28db4a22ef
le tu <lethucthanhtu@gmail.com>
2024-06-30 17:03:19 +0700

    lethucthanhtu: add default img

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -2,7 +2,12 @@ export const BASE_URL = import.meta.env.VITE_BASE_URL_DEV;
 const PREFIX = 'api/';
 
 export const CURR_USER_DATA_URL = `${PREFIX}users/user`;
+export const SUBJECT_URL        = `${PREFIX}course/categories`;
 export const LOGOUT_URL         = `${PREFIX}auth/logout`;
 export const LOGIN_URL          = `${PREFIX}auth/login`;
 export const GOOGLE_LOGIN_URL   = `${BASE_URL}${PREFIX}auth/login/google`;
 export const GOOGLE_SIGNUP_URL  = `${BASE_URL}${PREFIX}auth/register/google/`;
+export const COURSES_URL        = `${PREFIX}courses`;
+
+export const DEFAULT_IMG = `https://static.vecteezy.com/system/resources/thumbnails/020/765/399/small/default-profile-account-unknown-icon-black-silhouette-free-vector.jpg`;
+
